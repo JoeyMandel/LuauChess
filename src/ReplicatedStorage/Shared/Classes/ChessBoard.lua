1aec26c1d63b018a0b99cf717fa3660b5d221977
EnjoyableJelly <enjoyablejelly@gmail.com>
2021-04-09 23:54:59 -0400

    Implemented framework to ChessBoard

--- src/ReplicatedStorage/Shared/Classes/ChessBoard.lua
+++ src/ReplicatedStorage/Shared/Classes/ChessBoard.lua
@@ -10,6 +10,8 @@
 --]]
 
 --// Started Rewritting 3/29/2021
+local Knit = require(game:GetService("ReplicatedStorage").Knit)
+
 local Piece
 local Signal
 local Maid
@@ -82,8 +84,6 @@ function ChessBoard.new(state)
 	return self
 end
 
-
-
 function ChessBoard:Initiate()
 	
 end
@@ -109,8 +109,12 @@ function ChessBoard:ProcessUpdate(action)
 end
 
 function ChessBoard:AfterUpdate(action)
+	local pos1 = action[1].Orig
+	local pos2 = action[1].Target
+	local piece = self.State.Board[pos1]
+
 	print("[Client Board]: Piece Moved: "..BoardUtil.GetColor(piece.IsBlack).." "..piece.Type.." : "
-		..BoardUtil.ANFromVector2(action[1]).." -> "..BoardUtil.ANFromVector2(changes[2]))	
+		..BoardUtil.ANFromVector2(pos1).." -> "..BoardUtil.ANFromVector2(pos2))	
 
 	self.WhiteToNextMove = not self.WhiteToNextMove
 	self.MoveNumber += 0.5
@@ -124,40 +128,93 @@ function ChessBoard:CleanState()
 	local state = self.State
 	--// Clear legal moves, attacking and checking to prepare for next update
 	-- I imagine the board cleaning out all the gunk
-	for name,tbl in pairs(state.White) do
-		if name ~= "Pieces" then
-			local isChecking = (name == "Checking")
-			local isLegalMoves = (name == "LegalMoves")
-			local isAttacking = (name == "Attacking")
-
-			for index,val in pairs(tbl) do
-				if isChecking or isAttacking then
-					tbl[index] = nil
-				elseif isLegalMoves then
-					for i, _ in pairs(tbl[index]) do
-						val[i] = nil
+	local function clean(color)
+		for name,tbl in pairs(color) do
+			if name ~= "Pieces" then
+				local checking = (name == "Checking")
+				local legalMoves = (name == "LegalMoves")
+				local attacking = (name == "Attacking")
+	
+				for index,val in pairs(tbl) do
+					if checking or attacking then
+						tbl[index] = nil
+					elseif legalMoves then
+						for i, _ in pairs(tbl[index]) do
+							val[i] = nil
+						end
 					end
 				end
 			end
 		end
 	end
+	clean(self.State.White)
+	clean(self.State.Black)
 end
-
---// State
-
-function ChessBoard:Get(index)
-	return self.State[index]
-end
-
-function ChessBoard:Set(index,newVal)
-	self.State[index] = newVal
-end
-
 --// Other functions
 
+--[[
+	How a chess move works:
+		1. Moveinfo is found and determines if it is special or normal 
+			Note: Normal is one piece moving to another position
+			Special is when one piece moves to somewhere unexpected or moves/destroys another pieces
+		2. BeforeMove event is fired
+		3. MoveInfo is executed and the pieces are moved
+		4. Changes next move to the opp color
+		5. Confirms position by updating legal moves
+		6. Fires AfterMoved
+]]
+function ChessBoard:Move(pos1,pos2,config)
+	local board = self.State.Board
+	local piece =  board[pos1].Piece
+	local moveInfo = piece.LegalMoves[pos2]
 
-function ChessBoard:ANFromMoveInfo(moveInfo) --// Done after update
-	local firstPiece = BoardUtil.Get(self.Board,moveInfo[1]).Piece
+	if moveInfo and (not piece.IsBlack == self.WhiteToNextMove) then
+		if piece:HasTag("InputPossible") then
+			warn("Input changed Changes")
+			moveInfo = piece:ApplyInput(pos2,config)
+		end
+	
+		self.BeforeMoved:Fire(moveInfo)
+		piece:BeforeUpdate(moveInfo)
+		warn(self:ANFromMoveInfo(moveInfo))
+
+		for _,action in pairs(moveInfo) do
+			local actionType = action.Type
+			if actionType == "Move" then
+				local origPos = action.Orig
+				local targetPos = action.Target
+				
+				local origTile = board[origPos]
+				local targetTile = board[targetPos]
+				
+				local firstPiece = origTile.Piece
+				local targetPiece = targetTile.Piece
+
+				targetPiece:Destroy()
+				origTile.Piece = nil
+				targetTile.Piece = firstPiece
+			elseif actionType == "Create" then
+			
+			elseif  actionType == "Destroy" then
+				local targetPos = action.Target
+				local targetPiece = board[targetPos].Piece
+				targetPiece:Destroy()
+			end
+  		end
+
+		print("[Client Board]: Piece Moved: "..BoardUtil.GetColor(piece.IsBlack).." "..piece.Type.." : "
+			..BoardUtil.ANFromVector2(pos1).." -> "..BoardUtil.ANFromVector2(pos2))	
+
+		self.WhiteToNextMove = not self.WhiteToNextMove
+		self.MoveNumber += 0.5
+		self.State = "Idle"
+		self:UpdateMoves()
+		self.AfterMoved:Fire(moveInfo)
+	end
+end
+--[[
+function ChessBoard:ANFromMoveInfo(action) --// Done after update
+	local firstPiece = BoardUtil.Get(self.Board.,action[1].Orig).Piece
 
 	local color = BoardUtil.GetColor(firstPiece.IsBlack)	
 	local takenPiece = false
@@ -192,77 +249,7 @@ function ChessBoard:ANFromMoveInfo(moveInfo) --// Done after update
 	end
 	return str
 end
---[[
-	How a chess move works:
-		1. Moveinfo is found and determines if it is special or normal 
-			Note: Normal is one piece moving to another position
-			Special is when one piece moves to somewhere unexpected or moves/destroys another pieces
-		2. BeforeMove event is fired
-		3. MoveInfo is executed and the pieces are moved
-		4. Changes next move to the opp color
-		5. Confirms position by updating legal moves
-		6. Fires AfterMoved
 ]]
-function ChessBoard:Move(pos1,pos2,config)
-	local board = self:Get("Board")
-	local piece =  BoardUtil.Get(board,pos1).Piece
-	local moveInfo = BoardUtil.Get(piece.LegalMoves,pos2)
-
-	if moveInfo and (not piece.IsBlack == self.WhiteToNextMove) then
-		if piece:HasTag("InputPossible") then
-			warn("Input changed Changes")
-			moveInfo = piece:ApplyInput(pos2,config)
-		end
-	
-		self.BeforeMoved:Fire(moveInfo)
-		piece:BeforeUpdate(moveInfo)
-		warn(self:ANFromMoveInfo(moveInfo))
-
-		for _,action in pairs(moveInfo) do
-			local actionType = action.Type
-			if actionType == "Move" then
-				local origPos = action.Orig
-				local targetPos = action.Target
-				
-				local origTile = BoardUtil.Get(board,origPos)
-				local targetTile = BoardUtil.Get(board,targetPos)
-			elseif actionType == "Create" then
-			
-			elseif  actionType == "Destroy" then
-				
-			end
-  		end
-
-		for offset = 1,#changes,2 do
-			local origPos = changes[offset]
-			local targetPos = changes[offset+1]
-			
-			local origTile = BoardUtil.Get(board,origPos)
-			local origPiece = origTile.Piece
-
-			if targetPos then
-				local targetTile = BoardUtil.Get(board,targetPos)
-				if targetTile.Piece then
-					targetTile.Piece:Destroy()
-				end
-				targetTile.Piece = origPiece
-				targetTile.Piece.Position = targetPos
-				origTile.Piece = nil
-			else
-				origTile.Piece:Destroy()
-			end
-		end
-		print("[Client Board]: Piece Moved: "..BoardUtil.GetColor(piece.IsBlack).." "..piece.Type.." : "
-			..BoardUtil.ANFromVector2(changes[1]).." -> "..BoardUtil.ANFromVector2(changes[2]))	
-
-		self.WhiteToNextMove = not self.WhiteToNextMove
-		self.MoveNumber += 0.5
-		self.State = "Idle"
-		self:UpdateMoves()
-		self.AfterMoved:Fire(changes)
-	end
-end
-
 function ChessBoard:IsAttacking(color,pos)
 	local attacking = self[color].Attacking
 	return BoardUtil.Get(attacking,pos)
@@ -302,7 +289,7 @@ function ChessBoard:LoadFromFEN(FENString)
 	self.WhiteToNextMove = (fields[2] == "w" and true) or false
 end
 
-function ChessBoard:UpdateIfCheckmated(color)
+function ChessBoard:SetBoardState(color)
 	local colorTbl = self[color]
 	local hasLegalMoves = false
 	local isInCheck = #colorTbl.Checking > 0 
@@ -318,16 +305,17 @@ function ChessBoard:UpdateIfCheckmated(color)
 	end
 	if not hasLegalMoves and isInCheck then
 		warn("[Client Board]: ".. color.." has just been checkmated!")
-		self:UpdateState("Checkmate")
+		self:SetState("Checkmate")
 	elseif not hasLegalMoves and not isInCheck then
 		warn("[Client board]: "..color.." has been stalemated! How embarrassing...")
+		self:SetState("Stalemate")
 	end
 end
 
-function ChessBoard:UpdateState(newState)
+function ChessBoard:SetState(newState)
 	local currentVal = soundHierarchy[self.State]
 	local newVal = soundHierarchy[newState] or 0
-	if newVal > currentVal then
+	if newVal >= currentVal then
 		self.State = newState
 	end
 end
@@ -358,8 +346,8 @@ function ChessBoard:UpdateMoves()
 		piece:ComputeLegalMoves()
 	end
 	
-	self:UpdateIfCheckmated("Black")
-	self:UpdateIfCheckmated("White")
+	self:SetBoardState("Black")
+	self:SetBoardState("White")
 	
 	print("[Client Board]: All Legal Moves Updated")	
 end
@@ -379,12 +367,13 @@ function ChessBoard:CheckIfIsLegalMove(pos1,pos2)
 	return false
 end
 function ChessBoard:Init()
-	Signal = self.Shared.Utils.Signal
-	Maid = self.Shared.Utils.Maid
-	Tile = self.Shared.Tile
-	StringUtil = self.Shared.Utils.StringUtil
-	Piece = self.Shared.Piece
-	BoardUtil = self.Shared.Utils.BoardUtil
+	local shared = Knit.Shared
+	Signal = require(shared.Utils.Signal)
+	Maid = require(shared.Utils.Maid)
+	Tile = require(shared.Tile)
+	StringUtil = require(shared.Utils.StringUtil)
+	Piece = require(shared.Piece)
+	BoardUtil = require(shared.Utils.BoardUtil)
 end
 
 return ChessBoard
