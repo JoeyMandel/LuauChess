446a775435fef29b84338adda8207af90e66b499
EnjoyableJelly <enjoyablejelly@gmail.com>
2021-07-12 12:44:18 -0400

    Board Util Updates

--- src/ReplicatedStorage/Shared/Lib/BoardUtil.lua
+++ src/ReplicatedStorage/Shared/Lib/BoardUtil.lua
@@ -3,51 +3,74 @@
 -- March 6, 2021
 
 --[[
-	
+	function BoardUtil.ANFromVector2(pos: Vector2): string
+		Returns the position in algebraic notation
+			Vector2.new(2,2) --> a2
+	function BoardUtil.GetColor(isBlack: boolean): string
+		Returns the the corresponding string. 
+			isBlack == true --> "Black"
+			isBlack == false --> "white"
+	function BoardUtil.GetPieceIndexFromArray(array: Array, piece: Piece):  number
+		Gets a piece from an array of pieces
+	function BoardUtil.IsPositionValid(pos: Vector2): boolean	
+		Checks if a position is within the bounds of an 8x8 chess board
+	function BoardUtil.GetX(int: number): number
+		Gets solely the X value from the int, when it is converted to a Vecto2
+	function BoardUtil.GetY(int: number): number
+		Gets solely the Y value from the int, when it is converted to a Vecto2
+	function BoardUtil.IntToVector2(intVal: number): Vector2
+		Takes in an int value and returns a Vector2
+	function BoardUtil.Vector2ToInt(valX: Vector2 | number, valY: number | nil): int 
+		Takes in either a Vector2 or two numbers and converts it to its corresponding int value.
+	function BoardUtil.FireRayInDirection(board: Array, orig: Vector2, dir: Vector2, collisionCheck: callBackFunction): Array
+		Adds dir to orig until the new position is out of bounds.
+		Returns an array of the values at the positions where the collisionCheck returned true
 
+		local Board = BoardUtil.Create(false)
+		BoardUtil.Set(Board, Vector2.new(4,4), true)
+		BoardUtil.Set(Board, Vector2.new(5,5), true)
+
+		BoardUtil.FireRayInDirection(Board, Vector2.new(1,1), Vector2.new(1,1), function(value)
+			return value
+		end)
+		print(Board) --> {[1] = true, [2] = true}
+		-- Typically your Board is populated with Tiles that give position information
+	function BoardUtil.FireRayToPoint(board: Array, orig: Vector2, target: Vector2, collisionCheck: callBackFunction): Array
+		Loops through all the positions between orig and target and calls the collisionCheck callBackFunction.
+		Returns an array of the values at the positions where the collisionCheck returned true
+
+		local Board = BoardUtil.Create(false)
+		BoardUtil.Set(Board, Vector2.new(4,4), true)
+		BoardUtil.Set(Board, Vector2.new(5,5), true)
+
+		BoardUtil.FireRayToPoint(Board, Vector2.new(1,1), Vector2.new(8,8), function(value)
+			return value
+		end)
+		print(Board) --> {[1] = true, [2] = true}
+		-- Typically your Board is populated with Tiles that give position information
+	function BoardUtil.Get(board: Array, pos: Vector2)
+		Returns the value on the inputted position
+	function BoardUtil.Set(board: Array, pos: Vector2, val: any)
+		Sets the position of the board to the inputted value
+	function BoardUtil.Create(defaultVal: any): Array
+		This function returns an empty array with all 8x8 slots preallocated and set to the default value
+	function BoardUtil.Reset(board: Array): Array
+		This function is somewhat redundant. It simply calls table.clear(board). 
 --]]
-
-
 
 local BoardUtil = {}
 
 local numToRank = {"a","b","c","d","e","f","g","h"}
-function BoardUtil.IsPositionValid(pos)	
-	if (pos.X <= 8 and pos.X >= 1 ) and (pos.Y <= 8 and pos.Y >= 1) then
-		return true
-	end
-	return false
-end
 
-function BoardUtil.GetX(int)
-	return ((int - int%9)/9) + 1
-end
-function BoardUtil.GetY(int)
-	return int%9
-end
-
-function BoardUtil.ANFromVector2(pos)
+function BoardUtil.ANFromVector2(pos: Vector2): string
 	return (numToRank[pos.X]..tostring(pos.Y))
 end
 
-function BoardUtil.IntToVector2(intVal)
-	local rank = intVal%9
-	local file = ((intVal - rank)/9) + 1
-	return Vector2.new(file,rank)   
-end
-
-function BoardUtil.Vector2ToInt(valX,valY) --// Allows you to use the X and Y values to create the pos. 
-	local y = typeof(valY) == "number" and valY or valX.Y
-	local x = typeof(valX) == "number" and valX or valX.X
-
-	return ((x-1)*9) + y  
-end
-
-function BoardUtil.GetColor(isBlack)
+function BoardUtil.GetColor(isBlack: boolean): string
 	return isBlack and "Black" or "White"
 end
 
-function BoardUtil.GetPieceIndexFromArray(array, piece)
+function BoardUtil.GetPieceIndexFromArray(array: Array, piece: Piece):  number
 	for index, otherPiece in pairs(array) do
 		if otherPiece.Id == piece.Id then
 			return index
@@ -55,8 +78,36 @@ function BoardUtil.GetPieceIndexFromArray(array, piece)
 	end
 	return 0
 end
+----
+function BoardUtil.IsPositionValid(pos: Vector2): boolean	
+	if (pos.X <= 8 and pos.X >= 1 ) and (pos.Y <= 8 and pos.Y >= 1) then
+		return true
+	end
+	return false
+end
 
-function BoardUtil.FireRayInDirection(board,orig, dir, collisionCheck)
+function BoardUtil.GetX(int: number): number
+	return ((int - int%9)/9) + 1
+end
+
+function BoardUtil.GetY(int: number): number
+	return int%9
+end
+
+function BoardUtil.IntToVector2(intVal: number): Vector2
+	local rank = BoardUtil.GetY(intVal)
+	local file = BoardUtil.GetX(intVal)
+	return Vector2.new(file,rank)   
+end
+
+function BoardUtil.Vector2ToInt(valX: Vector2 | number, valY: number | nil): int --// Allows you to use the X and Y values to create the pos. 
+	local y = typeof(valY) == "number" and valY or valX.Y
+	local x = typeof(valX) == "number" and valX or valX.X
+
+	return ((x-1)*9) + y  
+end
+
+function BoardUtil.FireRayInDirection(board: Array, orig: Vector2, dir: Vector2, collisionCheck: callBackFunction): Array
 	local collisions = {}
 
 	local delta_x = dir.X 
@@ -66,7 +117,6 @@ function BoardUtil.FireRayInDirection(board,orig, dir, collisionCheck)
 	local next = nil
 
 	local function getNext()
-		local c_X = offset.X + delta_x
 		return Vector2.new(math.floor(offset.X + delta_x), math.floor(offset.Y + delta_y))
 	end
 
@@ -84,7 +134,7 @@ function BoardUtil.FireRayInDirection(board,orig, dir, collisionCheck)
 	return collisions
 end
 
-function BoardUtil.FireRayToPoint(board, orig, target, collisionCheck)
+function BoardUtil.FireRayToPoint(board: Array, orig: Vector2, target: Vector2, collisionCheck: callBackFunction): Array
 	local collisions = {}
 
 	local delta_x = target.X - orig.X
@@ -106,24 +156,23 @@ function BoardUtil.FireRayToPoint(board, orig, target, collisionCheck)
 	return collisions
 end
 
-function BoardUtil.Get(board,pos)
+function BoardUtil.Get(board: Array, pos: Vector2)
 	if BoardUtil.IsPositionValid(pos) then
 		return board[BoardUtil.Vector2ToInt(pos)]
 	end
 end
 
-function BoardUtil.Set(board,pos,val)
+function BoardUtil.Set(board: Array, pos: Vector2, val: any)
 	if BoardUtil.IsPositionValid(pos) then
 		board[BoardUtil.Vector2ToInt(pos)] = val
 	end
 end
 
-
-function BoardUtil.Create()
-	return BoardUtil.Reset({})
+function BoardUtil.Create(default: any): Array
+	return table.create(BoardUtil.Vector2ToInt(8,8), default)
 end
 
-function BoardUtil.Reset(board)
+function BoardUtil.Reset(board: Array): Array
 	table.clear(board)
 	return board
 end
--- src/ReplicatedStorage/Shared/Lib/BoardUtil.spec.lua
+++ src/ReplicatedStorage/Shared/Lib/BoardUtil.spec.lua
@@ -5,14 +5,14 @@ return function()
     local ChessBoard = require(Knit.Shared.Classes.ChessBoard)
 
     local board = ChessBoard.new()
-    describe("FireRayToPoint", function()
+    describe("FireRayToPoint & ChessBoard", function()
         local collisions = BoardUtil.FireRayToPoint(board.Board:getState(), Vector2.new(1,1), Vector2.new(7,7), function(tile)
                 return tile.IsDark == true
             end)
         it("Should return a table", function()
             expect(typeof(collisions)).to.equal("table")
         end)
-        it("All collissions should be tiles", function()
+        it("All collisions should be tiles", function()
             local allTiles = true
             for _, tile in pairs(collisions) do
                 if typeof(tile) ~= "table" or tile.Class ~= "Tile" then
@@ -47,14 +47,14 @@ return function()
             expect(allLarger).to.equal(true)
         end)
     end)
-    describe("FireRayInDirection", function()
+    describe("FireRayInDirection & ChessBoard", function()
         local collisions = BoardUtil.FireRayInDirection(board.Board:getState(), Vector2.new(1,1), Vector2.new(1,1), function(tile)
                 return tile.IsDark == true
             end)
         it("Should return a table", function()
             expect(typeof(collisions)).to.equal("table")
         end)
-        it("All collissions should be tiles", function()
+        it("All collisions should be tiles", function()
             local allTiles = true
             for _, tile in pairs(collisions) do
                 if typeof(tile) ~= "table" or tile.Class ~= "Tile" then
@@ -87,6 +87,64 @@ return function()
                 end
             end
             expect(allLarger).to.equal(true)
+        end)
+    end)
+    describe("Ray Testings (no other dependencies)", function()
+        it("FireRayInDirection returns the correct values", function()
+            local Board = BoardUtil.Create(false)
+            BoardUtil.Set(Board, Vector2.new(4,4), true)
+            BoardUtil.Set(Board, Vector2.new(5,5), true)
+
+            local collisions = BoardUtil.FireRayInDirection(Board, Vector2.new(1,1), Vector2.new(1,1), function(value)
+                return value
+            end)
+            print(collisions) --> {[1] = true, [2] = true}
+            expect(#collisions).to.equal(2)
+        end)
+        it("FireRayToPoint returns the correct values", function()
+            local Board = BoardUtil.Create(false)
+            BoardUtil.Set(Board, Vector2.new(4,4), true)
+            BoardUtil.Set(Board, Vector2.new(5,5), true)
+    
+            local collisions = BoardUtil.FireRayToPoint(Board, Vector2.new(1,1), Vector2.new(8,8), function(value)
+                return value
+            end)
+            print(collisions) --> {[1] = true, [2] = true}
+            expect(#collisions).to.equal(2)
+        end)
+    end)
+    describe("Proper Vector2 To Int Utils", function()
+        it("Converting back and forth should return the same value", function()
+            local orig = Vector2.new(8,8)
+            local int = BoardUtil.Vector2ToInt(orig)
+            expect(BoardUtil.IntToVector2(int)).to.equal(orig)
+        end)
+        it("All indexes must be unique", function()
+            local array = BoardUtil.Create()
+
+            local indexAlreadyExists = false
+
+            for x = 1, 8 do
+                for y = 1, 8 do
+                    local pos = BoardUtil.Vector2ToInt(x,y)
+                    if array[pos] then
+                        indexAlreadyExists = true
+                        break
+                    end
+                    array[pos] = true
+                end
+            end
+            expect(indexAlreadyExists).to.equal(false)
+        end)
+        it("Getting the X value of an int equals the original value", function()
+            local orig = Vector2.new(8,5)
+            local int = BoardUtil.Vector2ToInt(orig)
+            expect(BoardUtil.GetX(int)).to.equal(orig.X)
+        end)
+        it("Getting the Y value of an int equals the original value", function()
+            local orig = Vector2.new(8,5)
+            local int = BoardUtil.Vector2ToInt(orig)
+            expect(BoardUtil.GetY(int)).to.equal(orig.Y)
         end)
     end)
 end
