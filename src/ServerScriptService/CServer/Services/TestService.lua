676bf50fcdbf362cd92c80726bca289c0353e611
Joey Mandel <joeymandelprogamming@gmail.com>
2023-08-22 01:38:58 -0400

    Added BoardLoader Test

--- src/ServerScriptService/CServer/Services/TestService.lua
+++ src/ServerScriptService/CServer/Services/TestService.lua
@@ -1,5 +1,7 @@
 local Knit = require( game:GetService("ReplicatedStorage").Knit)
 
+local ChessGameClass = require(Knit.Shared.Classes.ChessGameClass)
+
 local TestService = Knit.CreateService({
     ["Name"] = "TestService",
     ["Client"] = {},
@@ -7,7 +9,9 @@ local TestService = Knit.CreateService({
 
 
 function TestService:KnitStart()
-    
+    local startPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    local game = ChessGameClass.new()
+    game:Play(startPosition)
 end
 
 
